6a8930f3674c22ad91091b222dba7199773ff3da
S20035\변승현 <s20035@gsm.hs.kr>
2021-03-28 00:48:42 +0900

    20210327

---
+++ app/src/main/java/com/example/bmi_calculator/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bmi_calculator
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
